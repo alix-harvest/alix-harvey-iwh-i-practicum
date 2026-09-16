7659b11c33cb0e3b7bc48c67184dc7211e3182ab
Alix Harvey <alix@harvestroi.com>
2026-09-16 05:47:32 -0700

    Read and create dachshunds using Contacts

--- index.js
+++ index.js
@@ -8,67 +8,52 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
+const headers = {
+    Authorization: `Bearer ${process.env.HUBSPOT_SERVICE_KEY}`,
+    'Content-Type': 'application/json'
+};
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/', async (req, res) => {
+    try {
+        const data = [];
+        let after;
+        do {
+            const response = await axios.get(contacts, {
+                headers,
+                params: { properties: 'dachshund_name,color,personality', limit: 100, after }
+            });
+            data.push(...response.data.results.filter(record => record.properties.dachshund_name));
+            after = response.data.paging?.next?.after;
+        } while (after);
+        res.render('homepage', { title: 'Mini Dachshunds | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error('Could not load Mini Dachshunds:', error.response?.status, error.response?.data?.message || error.message);
+        res.status(500).send('Could not load Mini Dachshunds. Please try again.');
+    }
+});
 
 app.get('/update-cobj', (req, res) => {
     res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+app.post('/update-cobj', async (req, res) => {
+    const newRecord = {
         properties: {
-            "favorite_book": req.body.newVal
+            dachshund_name: req.body.name,
+            // Contacts require a first name, last name, or email.
+            firstname: req.body.name,
+            color: req.body.color,
+            personality: req.body.personality
         }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
     };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        await axios.post(contacts, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Could not create Mini Dachshund:', error.response?.status, error.response?.data?.message || error.message);
+        res.status(500).send('Could not create Mini Dachshund. Please try again.');
     }
-
 });
-*/
 
-
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
